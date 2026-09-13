ef350c95e65fdde507a87c38b99500d67ed44379
Juniklx <marcelostj2014@gmail.com>
2026-09-12 23:12:01 -0300

    style: improve code formatting and consistency in header, sidebar, and auth guard components

--- src/app/guards/auth.guard.ts
+++ src/app/guards/auth.guard.ts
@@ -3,13 +3,13 @@ import { CanActivateFn, Router } from '@angular/router';
 import { Auth } from '../services/auth';
 
 export const authGuard: CanActivateFn = () => {
-    const auth = inject(Auth);
-    const router = inject(Router);
+  const auth = inject(Auth);
+  const router = inject(Router);
 
-    if (auth.estaLogado()) {
-        return true;
-    }
+  if (auth.estaLogado()) {
+    return true;
+  }
 
-    router.navigate(['/login']);
-    return false;
+  router.navigate(['/login']);
+  return false;
 };
